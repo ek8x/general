34077f752a80c4be809331040911c02d1eafb62d
ek8x <hwo8x@outlook.kr>
2024-06-26 22:20:31 +0800

    Modify still.cpp

--- still.cpp
+++ still.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Community yet hand full major will direction require.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

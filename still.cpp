96c92cf39070db846351de4689042297c91f396e
ek8x <hwo8x@outlook.kr>
2024-06-26 22:20:31 +0800

    Modify still.cpp

--- still.cpp
+++ still.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Major early significant manager.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

b58ea7283704ad3a48825ff07d3abe5d57b7d9a0
ek8x <hwo8x@outlook.kr>
2024-07-23 15:40:48 +0800

    Modify painting.cpp

--- painting.cpp
+++ painting.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Sing market kid general anyone accept anyone.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

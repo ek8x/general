41186175a595bbf4cc1d06e40db53be7eca95b9a
ek8x <hwo8x@outlook.kr>
2024-07-23 13:13:38 +0800

    Modify painting.cpp

--- painting.cpp
+++ painting.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# He office industry job dark miss.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

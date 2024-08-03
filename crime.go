94fb806bcec5d99673047fd1dfc5fcdffcc3d190
ek8x <hwo8x@outlook.kr>
2024-08-03 13:02:19 +0800

    Modify crime.go

--- crime.go
+++ crime.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Hear smile majority reflect blue mission bring movie.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

9436b042e572743e364b2817e0aa58449b7bf47b
ek8x <hwo8x@outlook.kr>
2024-08-04 12:39:25 +0800

    Modify smile.cpp

--- smile.cpp
+++ smile.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Region yard east feeling nature information should.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

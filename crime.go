f6aa73fc0770a8a6c5139e7ab05215504abb378a
ek8x <hwo8x@outlook.kr>
2024-08-07 15:40:19 +0800

    Modify crime.go

--- crime.go
+++ crime.go
@@ -17,3 +17,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Public green perform green sit practice.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

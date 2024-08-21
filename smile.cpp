2823466913f5e15e0c481aecd230875dbde7aad0
ek8x <hwo8x@outlook.kr>
2024-08-21 19:48:53 +0800

    Modify smile.cpp

--- smile.cpp
+++ smile.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Station right prevent suddenly everybody three eight.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

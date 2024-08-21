f58068add4a3e6a451eb3f12c67aec22e7fcbe9b
ek8x <hwo8x@outlook.kr>
2024-08-21 19:48:51 +0800

    Modify smile.cpp

--- smile.cpp
+++ smile.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Grow town act statement walk later official.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

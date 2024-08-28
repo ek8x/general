fdbb197771fb30ed5d017583df411bb3a86e35c2
ek8x <hwo8x@outlook.kr>
2024-08-28 15:55:13 +0800

    Modify smile.cpp

--- smile.cpp
+++ smile.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Institution source century various interesting deep against create.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

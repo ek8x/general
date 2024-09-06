8dc92066d80ecdc2b5a2a843ea1414227b437cb9
ek8x <hwo8x@outlook.kr>
2024-09-06 15:12:33 +0800

    Modify game.cpp

--- game.cpp
+++ game.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Issue reach property everyone the customer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

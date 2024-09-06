9ad9ef7870402d9245c4a52c96bd38c827570b8a
ek8x <hwo8x@outlook.kr>
2024-09-06 15:12:34 +0800

    Modify smile.cpp

--- smile.cpp
+++ smile.cpp
@@ -38,3 +38,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Room few though represent job performance.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

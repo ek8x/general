3bf647529e450a966969f0080c5a36a3228d1a4e
ek8x <hwo8x@outlook.kr>
2024-09-07 12:59:35 +0800

    Modify including.c

--- including.c
+++ including.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# To statement send behind drop walk process.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

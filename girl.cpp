1bb25f17f56933f42a5206b5b80cde38480dc8a6
ek8x <hwo8x@outlook.kr>
2024-09-29 13:34:19 +0800

    Modify girl.cpp

--- girl.cpp
+++ girl.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Environmental summer again rock.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

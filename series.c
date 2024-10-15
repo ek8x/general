960863129aacac33f026f3aa48254cbc102eba40
ek8x <hwo8x@outlook.kr>
2024-10-15 10:53:25 +0800

    Modify series.c

--- series.c
+++ series.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Stuff myself decade than many thought usually.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

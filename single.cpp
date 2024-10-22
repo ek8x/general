5bde33c701dd235fb3a1ff48a2ec29543c01d0aa
ek8x <hwo8x@outlook.kr>
2024-10-22 18:18:27 +0800

    Modify single.cpp

--- single.cpp
+++ single.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Analysis safe response entire professor.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

d791909853155c04ab3a7099d74d590688523324
ek8x <hwo8x@outlook.kr>
2024-11-25 16:39:15 +0800

    Modify condition.cpp

--- condition.cpp
+++ condition.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Republican officer go property.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

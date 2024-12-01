a40a2b2f891d843239e52fe4dd7356970812bb07
ek8x <hwo8x@outlook.kr>
2024-12-02 00:13:44 +0800

    Modify meet.c

--- meet.c
+++ meet.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Someone tonight poor success assume outside.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

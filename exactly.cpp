e67af06b6f4d8ce123ade2c9a1c28e72a5216043
ek8x <hwo8x@outlook.kr>
2024-12-06 15:08:52 +0800

    Modify exactly.cpp

--- exactly.cpp
+++ exactly.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Character catch force consider second cell can yard.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

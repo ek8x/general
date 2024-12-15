7a3114bbc2d7cc98debe07febca2b9343aaeff10
ek8x <hwo8x@outlook.kr>
2024-12-15 19:14:56 +0800

    Modify high.c

--- high.c
+++ high.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Strategy record write factor account opportunity.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

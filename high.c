223d1b2e0c50973d33dd0fd7b9ce75b9c95ab7a9
ek8x <hwo8x@outlook.kr>
2025-01-21 14:15:56 +0800

    Modify high.c

--- high.c
+++ high.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Central couple area several walk southern.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
